a2bce5f96f8ca03c7fbfaf01e7163c62a2976018
Jason Wu <wujiechen@gmail.com>
2017-12-30 00:03:57 -0500

    rails g model user name token email password_digest add_index :users, :token has_secure_password gem 'bcrypt'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171229170258) do
+ActiveRecord::Schema.define(version: 20171230044258) do
 
   create_table "products", force: :cascade do |t|
     t.text "name"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20171229170258) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "token"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token"], name: "index_users_on_token"
   end
 
 end
