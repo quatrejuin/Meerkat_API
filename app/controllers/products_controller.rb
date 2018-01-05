b76b3f80993a500bebe62a76227372e45d2d4866
Jason Wu <wujiechen@gmail.com>
2018-01-05 08:51:47 -0500

    Add Access-Control-Allow-Origin head for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,19 @@
 class ProductsController < ApiController
   before_action :require_login
+  before_action :cors_set_access_control_headers
+  skip_before_action :require_login, :only => [:route_options]
+
+  def route_options
+    cors_preflight_check
+  end
+  
+  def cors_set_access_control_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email'
+    response.headers['Access-Control-Max-Age'] = "1728000"
+  end
+
   def index
     @products = Product.all()
     render json: @products
