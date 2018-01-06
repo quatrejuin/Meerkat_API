2fb320cb27ffa169401127afd6945078ff8f7af5
Jason Wu <wujiechen@gmail.com>
2018-01-05 22:05:06 -0500

    Correct the CORS handling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,20 @@
 class ProductsController < ApiController
-  before_action :require_login
-  before_action :cors_set_access_control_headers
-  skip_before_action :require_login, :only => [:route_options]
-
-  def route_options
-    cors_preflight_check
-  end
+  before_action :require_login,:cors_preflight_check
+  after_action :cors_set_access_control_headers
+  skip_before_action :require_login, :only => [:cors_preflight_check]
   
   def cors_set_access_control_headers
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email'
-    response.headers['Access-Control-Max-Age'] = "1728000"
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end
+  
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      cors_set_access_control_headers
+      render plain: '',  content_type: 'text/plain'
+    end
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '*all', controller: 'products', action: 'cors_preflight_check', via: [:options]
   resources :products
 
   scope :format => true, :constraints => { :format => 'json' } do
