eb997772a0780c5f8541bf3a42702a766493c0e7
Michael Wang <michael@wonng.com>
2018-01-22 15:04:27 +1100

    add delet product

---
+++ app/graph/resolvers/delete_product.rb
@@ -0,0 +1,14 @@
+class Resolvers::removeProduct < GraphQL::Function
+  # arguments passed as "args"
+  argument :id, !types.Int
+  # return type from the mutation
+  type ProductType
+
+  # the mutation method
+  # _obj - is parent object, which in this case is nil
+  # args - are the arguments passed
+  # _ctx - is the GraphQL context (which would be discussed later)
+  def call(_obj, args, _ctx)
+    Product.find(args[:id]).destroy!
+  end
+end
